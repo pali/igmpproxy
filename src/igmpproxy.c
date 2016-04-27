4f27f662f195469a7030d7715ec51f7992244b6a
Victor Toni <victor.toni@gmail.com>
2016-04-27 21:12:57 +0200

    Replace call to setpgrp with setpgid
    Linux and FreeBSD use a different signature for setpgrp.
    Both support setpgid which replaces the now obsolete setpgrp and has the same signature on both systems.

--- src/igmpproxy.c
+++ src/igmpproxy.c
@@ -134,7 +134,7 @@ int main( int ArgCn, char *ArgVc[] ) {
 	    // Detach daemon from terminal
 	    if ( close( 0 ) < 0 || close( 1 ) < 0 || close( 2 ) < 0
 		 || open( "/dev/null", 0 ) != 0 || dup2( 0, 1 ) < 0 || dup2( 0, 2 ) < 0
-		 || setpgrp() < 0
+		 || setpgid( 0, 0 ) < 0
 	       ) {
 		my_log( LOG_ERR, errno, "failed to detach daemon" );
 	    }
